9ee4acc93ac7d9fed62e821fb578c0d8b90ec257
ntn11phm <ntn11phm@student.hig.se>
2015-02-26 11:23:19 +0100

    More stupid git

---
+++ src/ui/StupidPanel_2.java
@@ -0,0 +1,8 @@
+package ui;
+
+import javax.swing.JPanel;
+
+public class StupidPanel_2 extends JPanel {
+	private static final long serialVersionUID = 1L;
+	private String msg = "Extra stupid!!";
+}
