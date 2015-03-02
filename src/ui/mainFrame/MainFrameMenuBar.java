c14ccf840160eef78dd3b4008be420da8fa97b32
ntn11phm <ntn11phm@student.hig.se>
2015-03-02 14:56:35 +0100

    refactoring

--- src/ui/mainFrame/MainFrameMenuBar.java
+++ src/ui/mainFrame/MainFrameMenuBar.java
@@ -2,51 +2,44 @@ package ui.mainFrame;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 
 public class MainFrameMenuBar {
-	JMenuBar menuBar = new JMenuBar();
+	private JMenuBar menuBar = new JMenuBar();
+	private JMenu fileMenu = new JMenu("File");
+	private JMenu editMenu = new JMenu("Edit");
 
 	public MainFrameMenuBar() {
 		setMenuBar();
-
 	}
 
 	public JMenuBar getMenu(){
 		return menuBar;
 	}
+
 	private void setMenuBar() {
-		//JMenuBar menuBar = new JMenuBar();
-		// setJMenuBar(menuBar);
-		JMenu fileMenu = new JMenu("File");
-		JMenu editMenu = new JMenu("Edit");
 		menuBar.add(fileMenu);
 		menuBar.add(editMenu);
+		addFileMenuItems();
+		addEditMenuItems();
+	}
 
+	private void addFileMenuItems() {
 		JMenuItem newAction = new JMenuItem("New");
 		JMenuItem openAction = new JMenuItem("Open");
 		JMenuItem saveAction = new JMenuItem("Save");
 		JMenuItem saveAsAction = new JMenuItem("Save As");
 		JMenuItem closeAction = new JMenuItem("Exit");
 
-		JMenuItem cutAction = new JMenuItem("Cut");
-		JMenuItem copyAction = new JMenuItem("Copy");
-		JMenuItem pasteAction = new JMenuItem("Paste");
-
 		fileMenu.add(newAction);
 		fileMenu.add(openAction);
 		fileMenu.add(saveAction);
 		fileMenu.add(saveAsAction);
 		fileMenu.add(closeAction);
-
-		editMenu.add(cutAction);
-		editMenu.add(copyAction);
-		editMenu.add(pasteAction);
-
+		
 		newAction.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				JOptionPane.showMessageDialog(newAction, "New!");
@@ -77,7 +70,15 @@ public class MainFrameMenuBar {
 
 			}
 		});
-
 	}
+	
+	private void addEditMenuItems() {
+		JMenuItem cutAction = new JMenuItem("Cut");
+		JMenuItem copyAction = new JMenuItem("Copy");
+		JMenuItem pasteAction = new JMenuItem("Paste");
 
+		editMenu.add(cutAction);
+		editMenu.add(copyAction);
+		editMenu.add(pasteAction);
+	}
 }
