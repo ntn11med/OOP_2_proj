48db9b7958d32e11d1befe4bd5254d347571e020
ntn11phm <ntn11phm@student.hig.se>
2015-03-09 16:08:45 +0100

    misspelled

--- src/model/ClassData.java
+++ src/model/ClassData.java
@@ -7,5 +7,13 @@ public class ClassData {
 	private String name;
 	private List<String> attributes = new LinkedList<String>();
 	private List<String> methods = new LinkedList<String>();
-	private ClassState state;
+	private ClassState role;
+	
+	public ClassData() {
+		this(new ClassRole());
+	}
+	
+	public ClassData(ClassState cs) {
+		role = cs;
+	}
 }
