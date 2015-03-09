ead5e5b744e812a0baf1a0031b334cea4b167383
ntn11phm <ntn11phm@student.hig.se>
2015-03-09 14:14:40 +0100

    updated componentFactory

--- src/graphicComponents/ComponentFactory.java
+++ src/graphicComponents/ComponentFactory.java
@@ -1,5 +1,19 @@
 package graphicComponents;
 
-public class ComponentFactory {
+import java.util.HashMap;
+import java.util.Map;
 
+public class ComponentFactory {
+	private Map<String, Point> factoryMap;
+	
+	
+	public ComponentFactory() {
+		this.factoryMap = new HashMap<String, Point>();
+		factoryMap.put("Class", new ClassComponent(0, 0));
+		factoryMap.put("Connector", new Connector(0, 0, 0, 0));
+	}
+	
+	public Point getComponent(String input) {
+		return factoryMap.get(input);
+	}
 }
