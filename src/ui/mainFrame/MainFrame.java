a7f9df89415656690763c423064835025b36441b
ntn11phm <ntn11phm@student.hig.se>
2015-04-01 11:28:54 +0200

    cleanup merge

--- src/ui/mainFrame/MainFrame.java
+++ src/ui/mainFrame/MainFrame.java
@@ -4,20 +4,16 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 
-<<<<<<< HEAD
 import figureComponenets.FigureMap;
-import figureComponenets.MouseInteraction;
-import figureComponenets.FigureList;
-import figureComponenets.FigureViewer;
-=======
 import figureComponenets.MouseInteraction;
 import figureComponenets.FigureList;
 import figureComponenets.FigureViewer;
 import graphicComponents.ComponentFactory;
 import graphicComponents.ComponentFactorySingleton;
 import graphicComponents.Text;
->>>>>>> origin/master
 import ui.mainFrame.MainFrameMenuBar;
+import ui.propertiesBar.PropertiesBar;
+import ui.symbolBar.SymbolBar;
 import ui.tabbedPane.TabbedPane;
 
 public class MainFrame extends JFrame {
@@ -26,15 +22,10 @@ public class MainFrame extends JFrame {
 	private FigureMap figureMap = new FigureMap();
 	private FigureViewer figureViewer = new FigureViewer(figureMap);
 	private MainFrameMenuBar menuBar = new MainFrameMenuBar();
-<<<<<<< HEAD
 	private SymbolBar sb = new SymbolBar(figureMap);
 	private PropertiesBar pb = new PropertiesBar();
-=======
 	private TabbedPane tp = new TabbedPane();
-	// private SymbolBar sb = new SymbolBar(figureList);
-	// private PropertiesBar pb = new PropertiesBar();
 	private ComponentFactory cf;
->>>>>>> origin/master
 
 	public MainFrame() {
 		cf = ComponentFactorySingleton.getInstance();
@@ -56,29 +47,20 @@ public class MainFrame extends JFrame {
 	private void setBorderLayout() {
 		JPanel panel = new JPanel();
 		panel.setLayout(null);
-		// Adding figures to panel
-		figureList.add(cf.getComponent("Class"));
-		// figureList.add(cf.getComponent("Connector"));
-		figureList.add(new Text(155, 155, "Klass namn"));
 		figureViewer.setBounds(0, 0, 5000, 5000);
 		JScrollPane jsp = new JScrollPane(figureViewer);
 		jsp.setBounds(200, 40, 1500, 1000);
 		panel.add(jsp);
-<<<<<<< HEAD
 		sb.setBounds(300, 0, 300, 40);
 		panel.add(sb);
-		pb.setBounds(0, 0, 200, 200);
-		panel.add(pb);
 		MouseInteraction mouseInteraction = new MouseInteraction(figureMap);
-=======
+
 		tp.setBounds(0, 0, 199, 914);
 		panel.add(tp);
 		// sb.setBounds(300, 0, 300, 40);
 		// panel.add(sb);
 		// pb.setBounds(0, 0, 200, 200);
 		// panel.add(pb);
-		MouseInteraction mouseInteraction = new MouseInteraction(figureList);
->>>>>>> origin/master
 		figureViewer.addMouseListener(mouseInteraction);
 		figureViewer.addMouseMotionListener(mouseInteraction);
 		setContentPane(panel);
