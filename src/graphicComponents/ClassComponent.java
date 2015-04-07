becdc2c78346aa68037903be6fbaad5f5febb4e3
ntn11phm <ntn11phm@student.hig.se>
2015-04-07 10:04:40 +0200

    adding text to viewComponents

--- src/graphicComponents/ClassComponent.java
+++ src/graphicComponents/ClassComponent.java
@@ -1,7 +1,9 @@
 package graphicComponents;
 
 import java.awt.Graphics;
+
 import model.ClassData;
+import model.ClassRole;
 
 public class ClassComponent extends Point {
 	private int headHeight;
@@ -10,7 +12,7 @@ public class ClassComponent extends Point {
 	private int width;
 	private int dx, dy;
 	private boolean isSelected;
-	private ClassData cd;
+	private ClassData cd = new ClassData("Class");
 	private final int HEAD_HEIGHT = 30;
 	private final int FONT_HEIGHT = 8;
 	private final int FONT_WIDTH = 5;
@@ -38,8 +40,8 @@ public class ClassComponent extends Point {
 	
 	private void calcuateBorders(ClassData data) {
 		this.headHeight = HEAD_HEIGHT;
-		this.attributeHeight = data.getAttributes().size() * FONT_HEIGHT;
-		this.methodHeight = data.getMethods().size() * FONT_HEIGHT;
+		this.attributeHeight = data.getAttributes().size() * (5 + FONT_HEIGHT);
+		this.methodHeight = data.getMethods().size() * (5 + FONT_HEIGHT);
 		this.width = data.getMaxLineLenght() * FONT_WIDTH;
 		cd = data;
 	}
@@ -50,6 +52,16 @@ public class ClassComponent extends Point {
 	
 	@Override
 	protected void drawSpecific(Graphics g) {
+		if (cd.getRole().equals("interface")) {
+			g.drawString("<< interface >>", dx + 5, dy + 5 + FONT_HEIGHT);
+		} else {
+			g.drawString("Hmmm", dx + 5, dy + 5 + FONT_HEIGHT);
+		}
+		g.drawString(cd.getName(), dx + 5, dy + 15 + 2 * FONT_HEIGHT);
+		for (int i = 0; i < cd.getAttributes().size(); i++)
+			g.drawString(cd.getAttributes().get(i), dx + 5, dy + 30 + FONT_HEIGHT * (i + 1) );
+		for (int i = 0; i < cd.getMethods().size(); i++)
+			g.drawString(cd.getMethods().get(i), dx + 5, dy + 30 + attributeHeight + FONT_HEIGHT * (i + 1) );
 		g.drawRect(dx, dy, width, headHeight);
 		g.drawRect(dx, dy+headHeight, width, attributeHeight);
 		g.drawRect(dx, dy+headHeight+attributeHeight, width, methodHeight);
--- src/model/ClassData.java
+++ src/model/ClassData.java
@@ -4,7 +4,7 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class ClassData {
-	private String name;
+	private String name = "";
 	private List<String> attributes = new LinkedList<String>();
 	private List<String> methods = new LinkedList<String>();
 	private ClassState role;
